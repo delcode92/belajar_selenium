2c4ccfa91102926ff29e5d2eca94a9431d69fa7c
meong92 <38969204+meong92@users.noreply.github.com>
2024-06-29 15:53:10 +0700

    up

--- selenium_get_page_source.py
+++ selenium_get_page_source.py
@@ -15,17 +15,37 @@ def main():
     
     driver = webdriver.Firefox(options=chrome_options)
 
+    # driver.get( "https://itam.acehprov.go.id/login" )
+    
     driver.get( "https://belajar-nextjs-alfi.vercel.app/login" )
+   
     
-    WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "username"))
-    )
 
+    # WebDriverWait(driver, 10).until(
+    #     EC.presence_of_element_located((By.ID, "username"))
+    # )
 
+    # give time to render the content
     time.sleep(5)
-    
-    html_source = driver.page_source
 
+    print("find element: ", driver.find_element(By.ID, "username") )
+    print("URL before login: ", driver.current_url)
+    # time.sleep(5)
+   
+    # Now you can interact with the page
+    username_input = driver.find_element(By.ID, "username")
+    password_input = driver.find_element(By.ID, "password")
+    submit_button = driver.find_element(By.TAG_NAME, "button")
+
+    username_input.send_keys("Admin")
+    password_input.send_keys("admin")
+    submit_button.click()
+
+    print("URL after login: ", driver.current_url)
+    # time.sleep(5)
+
+    html_source = driver.page_source
+    #
     print(html_source)
     driver.quit()
 
